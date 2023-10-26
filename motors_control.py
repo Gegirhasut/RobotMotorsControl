c09778edf7e8b341b15315ba4897694e0ff25356
Max ROS <max077@mail.ru>
2023-10-26 17:59:02 +0000

    New super egnine feature, pycache to gitignore

--- motors_control.py
+++ motors_control.py
@@ -6,5 +6,8 @@ class Robot:
     def engine_start(self):
         print('engine started')
 
+    def engine_mode_turbo(self):
+        print('engine turbo started')
+
     def engine_stop(self):
         print('engine stop')
